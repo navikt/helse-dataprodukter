7ddd35f8ae25adb492f146015dfe4aed34721199
Eirik Gallefoss <eirik.gallefoss@nav.no>
2022-12-08 13:15:02 +0100

    Initial commit

---
+++ forstegangssoknad/build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    application
+}
+group = "no.nav.helse"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+
+tasks {
+    jar {
+
+        archiveFileName.set("app.jar")
+
+        manifest {
+            attributes["Main-Class"] = "no.nav.helse.MainKt"
+            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+        }
+
+        doLast {
+            configurations.runtimeClasspath.get()
+                .filter { it.name != "app.jar" }
+                .forEach {
+                    val file = File("$buildDir/libs/${it.name}")
+                    if (!file.exists())
+                        it.copyTo(file)
+                }
+        }
+    }
+}
