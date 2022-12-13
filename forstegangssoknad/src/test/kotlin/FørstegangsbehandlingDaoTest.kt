c4a22ad85e5ba9adac9685fd54a9764be2afbbd9
Sondre Halvorsen <sondre.halvorsen@nav.no>
2022-12-13 16:11:06 +0100

    Fix test

--- forstegangssoknad/src/test/kotlin/FørstegangsbehandlingDaoTest.kt
+++ forstegangssoknad/src/test/kotlin/FørstegangsbehandlingDaoTest.kt
@@ -40,8 +40,8 @@ internal class FørstegangsbehandlingDaoTest {
         dao.lagreSøknad(personRef, testSøknad, true)
         dao.lagreSøknad(personRef, testSøknad2, false)
         val søkander = dao.hentSøknader(personRef)
-        assertEquals(testSøknad, søkander[0]) {"Wrong søknad: ${søkander[0]} should be ${testSøknad.id}"}
-        assertEquals(testSøknad2, søkander[1]) {"Wrong søknad: ${søkander[1]} should be ${testSøknad2.id}"}
+        assertEquals(testSøknad.id, søkander[0].id) {"Wrong søknad: ${søkander[0]} should be ${testSøknad.id}"}
+        assertEquals(testSøknad2.id, søkander[1].id) {"Wrong søknad: ${søkander[1]} should be ${testSøknad2.id}"}
     }
 }
 
