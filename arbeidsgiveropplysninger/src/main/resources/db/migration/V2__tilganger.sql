2a6d7d3b08a6749356df599736f2667bcc58d0eb
Marte Tårnes <marte.tarnes@nav.no>
2023-03-01 10:54:27 +0100

    Gir databasetilgang til gcp IAM-brukere
    Co-authored-by: Gustav Berggren <gustav.berggren@nav.no>

---
+++ arbeidsgiveropplysninger/src/main/resources/db/migration/V2__tilganger.sql
@@ -0,0 +1,13 @@
+DO
+$$
+BEGIN
+        IF EXISTS
+            (SELECT 1 FROM pg_roles WHERE rolname = 'cloudsqliamuser')
+        THEN
+            GRANT ALL PRIVILEGES ON TABLE public.flyway_schema_history TO cloudsqliamuser;
+            GRANT ALL PRIVILEGES ON TABLE public.haandtert_inntektsmelding TO cloudsqliamuser;
+            ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO cloudsqliamuser;
+            ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO cloudsqliamuser;
+END IF;
+END
+$$;
