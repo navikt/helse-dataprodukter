77f86c9139648f3fffa483aeb9d36b158c4d1902
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2023-03-10 12:44:47 +0100

    se her da, se her!

--- arbeidsgiveropplysninger/src/main/resources/db/migration/V4__datastream_publication.sql
+++ arbeidsgiveropplysninger/src/main/resources/db/migration/V4__datastream_publication.sql
@@ -1,1 +1,11 @@
-CREATE PUBLICATION dataprodukter_arbeidsgiveropplysninger_publication for ALL TABLES;
+DO
+$$
+    BEGIN
+        if not exists
+            (select 1 from pg_publication where pubname = 'dataprodukter_arbeidsgiveropplysninger_publication')
+        then
+            CREATE PUBLICATION dataprodukter_arbeidsgiveropplysninger_publication for ALL TABLES;
+        end if;
+    end;
+$$;
+
--- arbeidsgiveropplysninger/src/main/resources/db/migration/V5__datastream_replication.sql
+++ arbeidsgiveropplysninger/src/main/resources/db/migration/V5__datastream_replication.sql
@@ -1,1 +1,10 @@
-SELECT PG_CREATE_LOGICAL_REPLICATION_SLOT ('dataprodukter_arbeidsgiveropplysninger_replication', 'pgoutput');
+DO
+$$
+    BEGIN
+        if not exists
+            (select 1 from pg_replication_slots where slot_name = 'dataprodukter_arbeidsgiveropplysninger_replication')
+        then
+            PERFORM PG_CREATE_LOGICAL_REPLICATION_SLOT ('dataprodukter_arbeidsgiveropplysninger_replication', 'pgoutput');
+        end if;
+    end;
+$$;
