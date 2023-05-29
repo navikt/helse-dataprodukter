bf3c876ed492b97d2c44e9fe049e4b60b1e8e8df
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-05-29 19:46:14 +0200

    Slett søknad i db som er problematisk (i dev)

---
+++ forstegangsbehandling/src/main/resources/db/migration/V7__fjern_ugyldig_søknad_i_dev.sql
@@ -0,0 +1,5 @@
+DELETE FROM soknad
+WHERE
+        hendelse_id = 'b44e0111-2743-4828-9a94-59813f4f65a3' AND
+        soknad_id = '3eb3f151-9c27-4eb0-8f03-43bb2c10a411' AND
+        sykmelding_id = '04ddea04-afdc-4bc3-b69d-22d213857a3e';
