d567230eff65c2fcad293a3889055321b4e0c4dd
Eirik Gallefoss <eirik.gallefoss@nav.no>
2023-07-24 14:22:21 +0200

    Slett søknad med tom før fom siden den lager problemer når forlengelsene kommer inn

---
+++ forstegangsbehandling/src/main/resources/db/migration/V9__fjern_ugyldig_søknad_i_prod.sql
@@ -0,0 +1,5 @@
+DELETE FROM soknad
+WHERE
+        hendelse_id = '0fb6b1a5-3835-4dd7-8a92-8a8eb858920b' AND
+        soknad_id = '64fd36a7-80de-343d-b895-d54f1dde8df8' AND
+        sykmelding_id = '1935391a-f04f-4daf-8efe-ab183acba5ff';
