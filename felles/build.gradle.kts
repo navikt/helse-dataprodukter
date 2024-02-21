076bced9dee9b3c3ef24619c3f4305eef2c9882e
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2024-02-21 09:44:27 +0100

    Bump postgresql to 42.7.2

--- felles/build.gradle.kts
+++ felles/build.gradle.kts
@@ -7,5 +7,5 @@ dependencies {
     api("org.flywaydb:flyway-database-postgresql:10.8.1")
     api("com.github.seratch:kotliquery:1.9.0")
     api("com.zaxxer:HikariCP:5.1.0")
-    api("org.postgresql:postgresql:42.6.0")
+    api("org.postgresql:postgresql:42.7.2")
 }
