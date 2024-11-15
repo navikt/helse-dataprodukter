3c27a01159b1eb98795478b0f36e3b6e5b437d34
david steinsland <david@davidsteinsland.net>
2024-11-15 13:03:57 +0100

    bumper flyway og postgres

--- felles/build.gradle.kts
+++ felles/build.gradle.kts
@@ -1,5 +1,7 @@
 val rapidsAndRiversVersion = "2024111509181731658731.11009b44c672"
+val flywayVersion = "10.21.0"
 val hikariCPVersion = "6.1.0"
+val postgresqlVersion = "42.7.4"
 
 plugins {
     application
@@ -7,8 +9,8 @@ plugins {
 
 dependencies {
     api("com.github.navikt:rapids-and-rivers:$rapidsAndRiversVersion")
-    api("org.flywaydb:flyway-database-postgresql:10.8.1")
+    api("org.flywaydb:flyway-database-postgresql:$flywayVersion")
     api("com.github.seratch:kotliquery:1.9.0")
     api("com.zaxxer:HikariCP:$hikariCPVersion")
-    api("org.postgresql:postgresql:42.7.2")
+    api("org.postgresql:postgresql:$postgresqlVersion")
 }
