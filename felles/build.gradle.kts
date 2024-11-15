7315fdc5233c93e14e06f4ee9973d926d9494955
david steinsland <david@davidsteinsland.net>
2024-11-15 12:26:36 +0100

    bumper hikariCP

--- felles/build.gradle.kts
+++ felles/build.gradle.kts
@@ -1,4 +1,6 @@
 val rapidsAndRiversVersion = "2024111509181731658731.11009b44c672"
+val hikariCPVersion = "6.1.0"
+
 plugins {
     application
 }
@@ -7,6 +9,6 @@ dependencies {
     api("com.github.navikt:rapids-and-rivers:$rapidsAndRiversVersion")
     api("org.flywaydb:flyway-database-postgresql:10.8.1")
     api("com.github.seratch:kotliquery:1.9.0")
-    api("com.zaxxer:HikariCP:5.1.0")
+    api("com.zaxxer:HikariCP:$hikariCPVersion")
     api("org.postgresql:postgresql:42.7.2")
 }
