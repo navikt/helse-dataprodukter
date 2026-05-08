906c09726184602771ee90048702e4229fe41c06
Christian Skovborg Gule <59872984+chsko@users.noreply.github.com>
2026-05-08 15:02:20 +0200

    Revert "💚 Hjelper det å flytte repositories-oppsett til settings?"
    This reverts commit aec8b3c077083e04aee91802fdba83492c517e71.

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,4 +31,21 @@ allprojects {
             systemProperty("junit.jupiter.execution.parallel.config.fixed.parallelism", "8")
         }
     }
+
+    // Sett opp repositories basert på om vi kjører i CI eller ikke
+    // Jf. https://github.com/navikt/utvikling/blob/main/docs/teknisk/Konsumere%20biblioteker%20fra%20Github%20Package%20Registry.md
+    repositories {
+        mavenCentral()
+        if (providers.environmentVariable("GITHUB_ACTIONS").orNull == "true") {
+            maven {
+                url = uri("https://maven.pkg.github.com/navikt/maven-release")
+                credentials {
+                    username = "token"
+                    password = providers.environmentVariable("GITHUB_TOKEN").orNull!!
+                }
+            }
+        } else {
+            maven("https://repo.adeo.no/repository/github-package-registry-navikt/")
+        }
+    }
 }
